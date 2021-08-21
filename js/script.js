57bfba7dd453b2999db36e2eab2538bb9137a56b
Nasirul-Islam <nasirulislam866@gmail.com>
2021-08-21 10:59:57 +0600

    Memory sum added

--- js/script.js
+++ js/script.js
@@ -1,63 +1,61 @@
-// Event Handler For Memory 
+// All ID is here
 const memory8Button = document.getElementById('memory8-button');
 const memory16Button = document.getElementById('memory16-button');
-const memoryCost = document.getElementById('memory-cost');
 
-memory8Button.addEventListener('click', function () {
-    memoryCost.innerText = 0;
-})
-
-memory16Button.addEventListener('click', function () {
-    memoryCost.innerText = 180;
-    updateTotal()
-})
-
-// Event Handler For Storage 
 const storage256Button = document.getElementById('storage256-button');
 const storage512Button = document.getElementById('storage512-button');
 const storage1TBButton = document.getElementById('storage1TB-button');
-const storageCost = document.getElementById('storage-cost');
 
+const delivery25Button = document.getElementById('delivery25-button');
+const delivery21Button = document.getElementById('delivery21-button');
+
+const bestCost = document.getElementById('best-cost');
+const memoryCost = document.getElementById('memory-cost');
+const storageCost = document.getElementById('storage-cost');
+const deliveryCost = document.getElementById('delivery-cost');
+const totalCost = document.getElementById('total-cost');
+
+// Event Handler For Memory 
+memory8Button.addEventListener('click', function () {
+    memoryCost.innerText = 0;
+    const totalMemoryCost = parseInt(bestCost.innerText) + parseInt(memoryCost.innerText);
+    totalCost.innerText = totalMemoryCost;
+})
+memory16Button.addEventListener('click', function () {
+    memoryCost.innerText = 180;
+    const totalMemoryCost = parseInt(bestCost.innerText) + parseInt(memoryCost.innerText);
+    totalCost.innerText = totalMemoryCost;
+})
+
+// Event Handler For Storage 
 storage256Button.addEventListener('click', function () {
     storageCost.innerText = 0;
 })
-
 storage512Button.addEventListener('click', function () {
     storageCost.innerText = 100;
     updateTotal()
 })
-
 storage1TBButton.addEventListener('click', function () {
     storageCost.innerText = 180;
     updateTotal()
 })
 
 // Event Handler For Delivery Option 
-const delivery25Button = document.getElementById('delivery25-button');
-const delivery21Button = document.getElementById('delivery21-button');
-const deliveryCost = document.getElementById('delivery-cost');
-
 delivery25Button.addEventListener('click', function () {
     deliveryCost.innerText = 0;
 })
-
 delivery21Button.addEventListener('click', function () {
     deliveryCost.innerText = 20;
 })
 
 // Total Cost
-const bestCost = document.getElementById('best-cost');
-const totalCost = document.getElementById('total-cost');
-// console.log(bestCost)
+// function updateTotal() {
+//     const bestCostSum = Number(bestCost.innerText);
+//     const memoryCostSum = Number(memoryCost.innerText);
+//     const totalMemoryCost = bestCostSum + memoryCostSum;
+//     totalCost.innerText = totalMemoryCost;
+// }
 
-
-
-function updateTotal() {
-    const bestCostSum = Number(bestCost.innerText);
-    const memoryCostSum = Number(memoryCost.innerText);
-    const totalMemoryCost = bestCostSum + memoryCostSum;
-    totalCost.innerText = totalMemoryCost;
-    // const price = document.getElementById(price + '-)
-}
-
+// const productTotal = document.getElementById(totalId)
+// productTotal.innerText = productNumberValue * price
 
